923fd832bdf149fe2583e1678ff9df26b59a02a8
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 21:25:04 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define lint long long
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string s;
+	cin>>s;
+	int i;
+	cin>>i;
+	cout<<s[i-1];
+
+	return 0;
+}
