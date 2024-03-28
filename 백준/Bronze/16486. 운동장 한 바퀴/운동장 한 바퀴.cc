73e4de0e24d5a644670f4e296f8a4c4d4323ebb0
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:58:27 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define lint long long
+#define Pie 3.141592
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cout.precision(8);
+
+    double a,b;
+    cin>>a>>b;
+
+    double res=2*a+2*Pie*b;
+
+    cout<<res;
+
+    return 0;
+}
