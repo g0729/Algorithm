b66374b0e9d680f015cf842132d2cd04f6b8eed7
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:43:40 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        cin >> a;
+
+        for (int i = 0; i < a; i++)
+        {
+            cout << "=";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
