6232eeec4483a29b10c3ce354699e329d1c23ce6
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:34:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 48 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+struct loc
+{
+	int x, y;
+};
+bool cmp(const loc& p1, const loc& p2)
+{
+	if (p1.x < p2.x)
+		return true;
+	else if (p1.x == p2.x)
+		return p1.y < p2.y;
+	else
+		return false;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	loc* arr = new loc[n];
+	for (int i = 0; i < n; i++)
+		cin >> arr[i].x >> arr[i].y;
+	sort(arr, arr + n, cmp);
+	for (int i = 0; i < n; i++)
+		cout << arr[i].x << " "<<arr[i].y << "\n";
+}
