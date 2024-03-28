5ef4e9c8ebc7b84d0b54980129b7fecb4e7e63fb
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:54:53 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll a, b;
+    cin >> a >> b;
+    if (a == b)
+        cout << "1";
+    else
+        cout << "0";
+    return 0;
+}
