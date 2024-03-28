a48a5d13d6789e7673e5e5955e98b52ea487441c
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 21:12:46 +0900

    [Bronze I] Title: 드높은 남산 위에 우뚝 선, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Bronze/21965. 드높은 남산 위에 우뚝 선/드높은 남산 위에 우뚝 선.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	vector<int> v(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin>>v[i];
+	}
+	
+	bool ascending=true;
+	bool check=true;
+	for (int i = 0; i < n-1; i++)
+	{
+		if(v[i]<v[i+1])
+		{
+			if(!ascending)
+			{
+				check=false;
+				break;
+			}
+		}
+		else if(v[i]>v[i+1])
+			ascending=false;
+		else
+		{
+			check=false;
+			break;
+		}
+	}
+	
+	if(check)
+		cout<<"YES";
+	else
+		cout<<"NO";
+	return 0;
+}
