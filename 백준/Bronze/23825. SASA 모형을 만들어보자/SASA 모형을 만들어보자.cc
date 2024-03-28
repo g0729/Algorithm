215e2bb3b1e6b5a36d8179669eb6316b9ad7d8fe
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 21:15:34 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define lint long long
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+int a,b;
+cin>>a>>b;
+cout<<min(a/2,b/2);
+return 0;
+}
