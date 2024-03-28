e35fd211f85bf8d83f8076970978b7ef843050ce
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:52:00 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    n == 0 ? cout << "YONSEI" : cout << "Leading the Way to the Future";
+    
+    return 0;
+}
