0c4b1673defcc0be8ba01ba4257a7274d237b7d2
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 21:02:40 +0900

    [Silver III] Title: 피보나치는 지겨웡~, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17175. 피보나치는 지겨웡～/피보나치는 지겨웡～.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<ll> v(n + 1);
+
+    v[0] = 1;
+    v[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        v[i] = (v[i - 1] + v[i - 2] + 1) % MOD;
+    }
+
+    cout << v[n];
+    
+    return 0;
+}
