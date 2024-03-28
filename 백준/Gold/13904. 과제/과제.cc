88528a015857cf9e2cf29065f49dced808907a40
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:44:00 +0900

    [Gold III] Title: 과제, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/13904. 과제/과제.cc
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin>>n;
+	vector<pair<int,int>>v(n);
+	for (int i = 0; i < n; i++)
+	{
+		int a,b;
+		cin>>a>>b;
+		v[i]={a,b};
+	}
+	sort(v.begin(),v.end());
+	int cur=0;
+	priority_queue<int,vector<int>,greater<int>>pq;
+	for (int i = 0; i < n; i++)
+	{
+		if(cur<v[i].first)
+		{
+			pq.push(v[i].second);
+			cur++;
+		}
+		else
+		{
+			if(pq.top()<v[i].second)
+			{
+				pq.pop();
+				pq.push(v[i].second);
+			}
+		}
+	}
+	int sum=0;
+	while(!pq.empty())
+	{
+		sum+=pq.top();
+		pq.pop();
+	}
+	cout<<sum;
+}
