4872a9f0515ba986e553637e1b54e143b9d9a7b9
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 19:37:21 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n-1; i++)
+	{
+		for (int q = 0; q < n - i - 1; q++)
+			cout << " ";
+		for (int q = 0; q < 2 * i + 1; q++)
+			cout << "*";
+		cout << endl;
+	}
+	for (int i = n; i > 0; i--)
+	{
+		for (int q = 0; q < n - i; q++)
+			cout << " ";
+		for (int q = 0; q < 2 * i -1 ; q++)
+			cout << "*";
+		
+		cout << endl;
+	}
+}
