7b84cfb434cafe8a3b3f0ad5b26070339f8465ee
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:47:12 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+
+string s;
+cin>>s;
+cout<<":fan::fan::fan:\n";
+cout<<":fan::"<<s<<"::fan:\n";
+cout<<":fan::fan::fan:";
+return 0;
+}
