19a0ffbb4e54683f7b0ef61c7ead8f87ae24c83a
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 19:32:52 +0900

    [Gold III] Title: 구간 나누기, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Gold/2228. 구간 나누기/구간 나누기.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dp[101][51];
+int sum[101];
+int n, m;
+
+int cal(int now, int nowarea)
+{
+    if (nowarea == 0)
+        return 0;
+    if (now <= 0)
+        return -100000000;
+    if (dp[now][nowarea] != -1)
+        return dp[now][nowarea];
+
+    dp[now][nowarea] = cal(now - 1, nowarea);
+
+    for(int i = 1;i<=now;i++)
+    {
+        dp[now][nowarea]=max(dp[now][nowarea],cal(i-2,nowarea-1)+sum[now]-sum[i-1]);
+    }
+    return dp[now][nowarea];
+}
+int main()
+{
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        int a;
+        cin >> a;
+        sum[i] = sum[i - 1] + a;
+    }
+    memset(dp, -1, sizeof(dp));
+    cout<<cal(n,m);
+}
