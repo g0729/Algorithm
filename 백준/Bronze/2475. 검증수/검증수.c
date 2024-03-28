59d72cc51975aca22f6ffb5073ce22f20563e73d
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 19:38:41 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+    int n;
+    int sum=0;
+    
+    for(int i = 0 ; i<5;i++)
+    {
+        scanf("%d",&n);
+        sum += n*n;
+    }
+    printf("%d",sum%10);
+}
