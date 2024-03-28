cd3142e2c5f2567e260cffc80287f286df06055e
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:58:20 +0900

    [Unrated] Title: 작도하자! - ①, Time: 0 ms, Memory: 2020 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Unrated/16484. 작도하자！ － ①/작도하자！ － ①.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    double a, b;
+    cin >> a >> b;
+    cout << fixed;
+    cout.precision(1);
+    cout << a / 2 - b;
+    return 0;
+}
