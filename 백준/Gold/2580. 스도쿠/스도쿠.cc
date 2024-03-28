a357ee48499b64c07dc34b9c08c0d4959eeea3a1
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 19:43:30 +0900

    [Gold IV] Title: 스도쿠, Time: 248 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<pair<int, int>> v;
+short arr[9][9];
+int o_cnt=0;
+void print()
+{
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			cout << arr[j][i] << " ";
+		}
+		cout << endl;
+	}
+}
+bool check(int x, int y)
+{
+	int ta = arr[x][y];
+	for (int i = 0; i < 9; i++)
+	{
+		if (i != x && arr[i][y] == ta)
+			return false;
+		if (i != y && arr[x][i] == ta)
+			return false;
+	}
+	for (int j = (y / 3) * 3; j < (y / 3) * 3 + 3; j++)
+		for (int i = (x / 3) * 3; i < (x / 3) * 3 + 3; i++)
+			if (j != y && i != x && arr[i][j] == ta)
+				return false;
+	return true;
+}
+void DFS(int n)
+{
+	if (n == o_cnt)
+	{
+		print();
+		exit(0);
+	}
+	for (int i = 1; i <= 9; i++)
+	{
+		arr[v[n].first][v[n].second] = i;
+		if (check(v[n].first, v[n].second))
+		{
+			DFS(n + 1);
+		}
+	}
+	arr[v[n].first][v[n].second] = 0;
+	return;
+}
+int main()
+{
+	for (int y = 0; y < 9; y++)
+		for (int x = 0; x < 9; x++)
+		{
+			cin >> arr[x][y];
+			if (arr[x][y] == 0)
+			{
+				v.push_back({ x,y });
+				o_cnt++;
+			}
+		}
+	DFS(0);
+}
