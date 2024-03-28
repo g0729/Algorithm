795ae106d6624372d2766b3e028cf256d806b1bb
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 19:50:49 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int a, b;
+
+	scanf("%d%d", &a, &b);
+
+	if (b >= 45)
+		printf("%d %d", a, b - 45);
+	else if (a > 0)
+		printf("%d %d", a - 1, b + 15);
+	else
+		printf("23 %d", b + 15);
+}
