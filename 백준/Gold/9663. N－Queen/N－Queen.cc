4509d2b0ad99c8e30a27ce64439bd4b69bd5da62
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:17:43 +0900

    [Gold IV] Title: N-Queen, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[16]={0,1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596,78232};
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<arr[n];
+}
