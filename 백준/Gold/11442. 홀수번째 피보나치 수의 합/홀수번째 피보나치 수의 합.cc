f37881ade6b813510621e168c000604624748a47
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:32:55 +0900

    [Gold II] Title: 홀수번째 피보나치 수의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/11442. 홀수번째 피보나치 수의 합/홀수번째 피보나치 수의 합.cc
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lint long long
+#define matrix vector<vector<lint>>
+#define MOD 1000000007
+matrix operator *(const matrix &a ,const matrix &b)
+{
+    int k= a.size();
+    matrix c(k,vector<lint>(k));
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            for (int q = 0; q < k; q++)
+            {
+                c[i][j]+=a[i][q]*b[q][j];
+            }
+            c[i][j]=c[i][j]%MOD;
+        }
+    }
+    return c;
+}
+int main()
+{
+    lint n;
+    cin>>n;
+    if(n%2==0)
+    {
+        n= n-1;
+    }
+    n=n+1;
+    matrix res={{1,0},{0,1}};
+    matrix temp={{1,1},{1,0}};
+    while(n>0)
+    {
+        if(n%2==1)
+        {
+            res= res*temp;  
+        }
+        temp = temp*temp;
+        n=n/2;
+    }
+    cout<<res[0][1];
+}
+
