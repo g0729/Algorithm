9c3f68d737dc3da41bf0640dc6a7894e8f6f140f
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 21:10:41 +0900

    [Bronze II] Title: 스시, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20494. 스시/스시.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+   int n;
+   cin >> n;
+   int res = 0;
+
+   for (int i = 0; i < n; i++)
+   {
+      string s;
+      cin >> s;
+
+      res += s.length();
+   }
+
+   cout << res;
+   return 0;
+}
