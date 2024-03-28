452fb82562f1b01848aaaf4d1c41792a65692fd3
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 19:52:39 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+
+	cin >> a >> b >> c;
+
+	if (a + b == c)
+		cout << a << "+" << b << "=" << c;
+	else if (a - b == c)
+		cout << a << "-" << b << "=" << c;
+	else if (a * b == c)
+		cout << a << "*" << b << "=" << c;
+	else if (a % b == 0 && a / b == c)
+		cout << a << "/" << b << "=" << c;
+	else if(a==b+c)
+		cout<<a<<"="<<b<<"+"<<c;
+	else if(a==b-c)
+	cout<<a<<"="<<b<<"-"<<c;
+	else if(a==b*c)
+		cout<<a<<"="<<b<<"*"<<c;
+	else
+		cout<<a<<"="<<b<<"/"<<c;
+}
