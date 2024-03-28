5f358cc0d8f28f27d21235846766a9c31c64960f
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 21:18:52 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    char a;
+    cin >> a;
+
+    if (a == 'N' || a == 'n')
+        cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+    return 0;
+}
