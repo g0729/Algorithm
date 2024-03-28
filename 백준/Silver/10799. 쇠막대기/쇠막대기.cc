69058f16b2a4cb5d697af2bc50ea738662034b69
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:22:36 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define lint long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    stack<char> s;
+    string stick;
+    cin >> stick;
+
+    int res = 0;
+
+    for (int i = 0; i < stick.length(); i++)
+    {
+        if (stick[i] == '(')
+        {
+            s.push(stick[i]);
+        }
+        else if (stick[i] == ')' && stick[i-1] == '(')
+        {
+            s.pop();
+            res+=s.size();
+        }
+        else
+        {
+            res++;
+            s.pop();
+        }
+    }
+
+    cout << res;
+    return 0;
+}
