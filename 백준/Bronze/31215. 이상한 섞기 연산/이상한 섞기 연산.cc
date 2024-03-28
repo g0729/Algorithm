b094af1232946dc1318aef09de2bb7ba1e824468
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 21:31:14 +0900

    [Bronze II] Title: 이상한 섞기 연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31215. 이상한 섞기 연산/이상한 섞기 연산.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n <= 2)
+            cout << "1";
+        else
+            cout << "3";
+        cout << "\n";
+    }
+    return 0;
+}
