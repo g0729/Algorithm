892efe320a4cc5a3f148267400311c73d563d523
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 19:38:02 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int res=0;
+	int now=0;
+	for (int i = 0; i < 10; i++)
+	{
+		int a,b;
+
+		cin>>a>>b;
+
+		now-=a;
+		now+=b;
+		res=max(res,now);
+	}
+	
+	cout<<res;
+}
