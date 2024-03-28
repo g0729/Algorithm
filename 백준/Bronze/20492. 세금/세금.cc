8127c4b6166e57e6e3da3a4c2a23f0802490fdb6
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 21:10:35 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    cout <<(int) (n * 0.78) << " " << (int)(n * 0.956);
+    return 0;
+}
