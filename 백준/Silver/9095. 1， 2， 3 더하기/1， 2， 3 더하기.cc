ec2d914665074440e87a8cde2b29a37802f6c8e8
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:14:54 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	int arr[11];
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[3] = 4;
+
+	for (int i = 4; i < 11; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+	}
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int t; 
+		cin >> t;
+		cout << arr[t] << endl;
+	}
+}
