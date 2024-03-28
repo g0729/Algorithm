747400875cffe94b82134f74639abb43fbe71a9e
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 21:18:39 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+   int a, b;
+   cin >> a >> b;
+
+   cout << (a * 8 + b * 3) - 28;
+   return 0;
+}
