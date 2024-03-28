a52eac75ed05d5b2ef4d44acd22fd6df0438791e
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 20:58:13 +0900

    [Bronze III] Title: 접시 안의 원, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16483. 접시 안의 원/접시 안의 원.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 987654321
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    double t;
+    cin >> t;
+    cout<<fixed;
+    cout.precision(0);
+
+    cout << pow(t / 2, 2);
+    return 0;
+}
