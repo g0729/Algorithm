8f6a29214948bf0e3775bba2972880e83d5fcfed
g0729 <151703074+g0729@users.noreply.github.com>
2024-03-28 21:21:57 +0900

    [Bronze III] Title: 연세여 사랑한다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 987654321
+#define lint long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    char c;
+
+    cin>>c;
+
+    c=abs(c-'I');
+
+    cout<<c+84;
+    return 0;
+}
