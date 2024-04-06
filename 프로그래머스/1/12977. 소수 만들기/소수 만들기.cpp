0691e29610c7404819088a12ab497dec7f9899f5
g0729 <151703074+g0729@users.noreply.github.com>
2024-04-06 18:07:04 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num){
+    if(num<=1)
+        return false;
+    for(int i =2 ; i*i<=num;i++){
+        if(num%i==0)
+            return false;
+    }
+    return true;
+}
+int solution(vector<int> nums) {
+    int answer = 0;
+
+    for(int i = 0 ; i<nums.size()-2;i++){
+        for(int j = i+1 ; j<nums.size()-1;j++){
+            for(int k =j+1;k<nums.size();k++){
+                if(isPrime(nums[i]+nums[j]+nums[k]))
+                    answer++;
+            }
+        }
+    }
+    return answer;
+}
