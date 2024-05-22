d2d7f310f5e13cc37a413f7436cdaaade4dcc21c
g0729 <151703074+g0729@users.noreply.github.com>
2024-05-22 11:16:07 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll n;
+    cin >> n;
+    if (n % 2 == 1)
+        cout << "SK";
+    else
+        cout << "CY";
+    return 0;
+}
