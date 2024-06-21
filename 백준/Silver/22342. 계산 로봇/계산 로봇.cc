c86bb5bb24f9dcf6b3eceaac1c5a134ec5c0b8d8
g0729 <151703074+g0729@users.noreply.github.com>
2024-06-21 16:11:38 +0900

    [Silver I] Title: 계산 로봇, Time: 124 ms, Memory: 48900 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/22342. 계산 로봇/계산 로봇.cc
+++ 백준/Silver/22342. 계산 로봇/계산 로봇.cc
@@ -4,8 +4,9 @@ using namespace std;
 #define MOD 1000000007
 #define MAX 100000
 #define INF 987654321
-int MAP[2000][2000];
-int dp[2000][2000];
+int weights[2000][2000];
+int save[2000][2000];
+int output[2000][2000];
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -20,36 +21,31 @@ int main()
         string s;
         cin >> s;
         for (int j = 0; j < m; j++)
-            MAP[i][j] = s[j] - '0';
+            weights[i][j] = s[j] - '0';
     }
 
     for (int i = 0; i < n; i++)
-        dp[i][0] = MAP[i][0];
+        output[i][0] = weights[i][0];
 
     for (int i = 1; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
+            int temp = 0;
 
-            if (i == m - 1)
-                dp[j][i] = dp[j][i - 1];
-            else
-            {
-                int temp = 0;
-                for (int k = max(j - 1, 0); k <= min(j + 1, n - 1); k++)
-                    temp = max(temp, dp[k][i - 1]);
-                dp[j][i] = temp + MAP[j][i];
-            }
+            for (int k = max(j - 1, 0); k <= min(j + 1, n - 1); k++)
+                temp = max(temp, output[k][i - 1]);
+
+            save[j][i] = temp;
+            output[j][i] = temp + weights[j][i];
         }
     }
 
     int answer = 0;
-
     for (int i = 0; i < n; i++)
-    {
-        answer = max(answer, dp[i][m - 1]);
-    }
+        answer = max(answer, save[i][m - 1]);
 
     cout << answer;
+
     return 0;
 }
