2a54bd1f42abf3270b770f7761d672c84e3eb1a4
g0729 <151703074+g0729@users.noreply.github.com>
2024-06-27 21:50:21 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 492 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    int answer = 1;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (v[i] >= v[i - 1])
+            answer++;
+    }
+
+    cout << answer;
+
+    return 0;
+}
