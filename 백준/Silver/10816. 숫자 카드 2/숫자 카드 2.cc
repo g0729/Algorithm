7ffd90e4ffe8cbf66832ad388a240c6a5f7d14a2
g0729 <151703074+g0729@users.noreply.github.com>
2024-06-29 16:49:27 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 288 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,72 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define MAX 500000
-int arr[MAX];
-int fin[MAX];
-int res[MAX];
-int n;
-
-int upper(int target)
-{
-    int start = 0;
-    int end=n-1;
-    while(start<end)
-    {
-        int mid =(start+end)/2;
-        if(arr[mid]>target)
-        {
-            end =mid;
-        }
-        else
-        start=mid+1;
-    }
-    return end;
-}
-int lower(int target)
-{
-    int start =0;
-    int end=n-1;
-    while(start<end)
-    {
-        int mid=(start+end)/2;
-        if(arr[mid]>=target)
-        end=mid;
-        else
-        start =mid+1;
-    }
-    return end;
-}
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    cin>>n;
-    for(int i = 0 ; i<n;i++)
-    {
-        cin>>arr[i];
-    }
-    sort(arr,arr+n);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    sort(v.begin(), v.end());
+
     int m;
-    cin>>m;
+    cin >> m;
     for (int i = 0; i < m; i++)
     {
-        cin>>fin[i];
+        int a;
+        cin >> a;
+        cout << upper_bound(v.begin(), v.end(), a) - lower_bound(v.begin(), v.end(), a) << " ";
     }
-    for(int i = 0 ; i<m;i++)
-    {
-        int up =upper(fin[i]);
-        int low =lower(fin[i]);
-        if(up==n-1&&arr[n-1]==fin[i])
-        up++;
 
-        res[i]=up-low;
-    }
-    for (int i = 0; i < m; i++)
-    {
-        cout<<res[i]<<" ";
-    }
-    
-
+    return 0;
 }
