199c9c97b242d8a7bb715472e7946f977dce4c4f
g0729 <151703074+g0729@users.noreply.github.com>
2024-07-25 16:23:42 +0900

    [Gold V] Title: 가로등, Time: 356 ms, Memory: 58528 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Gold/32069. 가로등/가로등.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
+ll n, l, k;
+vector<ll> v;
+int dx[] = {1, -1};
+void bfs()
+{
+    queue<ll> q;
+    set<ll> visited;
+    for (auto it : v)
+    {
+        q.push(it);
+        visited.insert(it);
+    }
+
+    int distance = 0;
+    int size;
+
+    while (k)
+    {
+        size = q.size();
+        while (size--)
+        {
+            ll now = q.front();
+            q.pop();
+
+            for (int i = 0; i < 2; i++)
+            {
+                int next = now + dx[i];
+
+                if (next < 0 || next > l)
+                    continue;
+                if (visited.find(next) != visited.end())
+                    continue;
+                q.push(next);
+                visited.insert(next);
+            }
+            cout << distance << "\n";
+            k--;
+            if (k == 0)
+                break;
+        }
+        distance++;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> l >> n >> k;
+
+    for (int i = 0; i < n; i++)
+    {
+        ll a;
+        cin >> a;
+        v.push_back(a);
+    }
+
+    bfs();
+
+    return 0;
+}
