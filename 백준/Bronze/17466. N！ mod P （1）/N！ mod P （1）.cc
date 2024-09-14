3126d909bd2ffef2f1a2b1ae5f24f431c36be9b7
g0729 <151703074+g0729@users.noreply.github.com>
2024-09-14 13:04:10 +0900

    [Bronze II] Title: N! mod P (1), Time: 1216 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n, p;
+    cin >> n >> p;
+    ll res = 1;
+    for (ll i = 1; i <= n; i++)
+    {
+        res = (res * i) % p;
+    }
+
+    cout << res;
+    return 0;
+}
