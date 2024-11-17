cb1b91a75271160455005de06a57fd5c4d8cf0aa
g0729 <151703074+g0729@users.noreply.github.com>
2024-11-17 13:05:25 +0900

    [Bronze II] Title: Virus Outbreak, Time: 116 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Bronze/15841. Virus Outbreak/Virus Outbreak.py
+++ 백준/Bronze/15841. Virus Outbreak/Virus Outbreak.py
@@ -1,10 +1,8 @@
-arr = [1, 1]
+l = [0, 1] + [0] * 490
 for i in range(2, 490):
-    arr.append(arr[i - 2] + arr[i - 1])
-
+    l[i] = l[i - 2] + l[i - 1]
 while True:
     n = int(input())
     if n == -1:
         break
-
-    print(f"Hour {n}: {arr[n-1]} cow(s) affected")
+    print(f"Hour {n}: {l[n]} cow(s) affected")
