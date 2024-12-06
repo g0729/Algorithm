866a258c194897f96aa54d5f00b9db0967733ac2
g0729 <151703074+g0729@users.noreply.github.com>
2024-12-06 18:43:57 +0900

    [Platinum III] Title: XOR 합, Time: 2344 ms, Memory: 50992 KB -BaekjoonHub

--- 백준/Platinum/13504. XOR 합/XOR 합.cc
+++ 백준/Platinum/13504. XOR 합/XOR 합.cc
@@ -6,59 +6,53 @@ using namespace std;
 #define INF 987654321
 int dx[] = {1, -1, 0, 0};
 int dy[] = {0, 0, 1, -1};
+
 struct Trie
 {
-    map<char, Trie *> ma;
-
+    Trie *node[2];
+    Trie()
+    {
+        node[0] = node[1] = NULL;
+    }
     ~Trie()
     {
-        for (auto it : ma)
-            delete it.second;
-    }
-    void insert(vector<char> &v, int idx)
-    {
-        if (idx == 32)
-            return;
-        if (ma.find(v[idx]) == ma.end())
-        {
-            Trie *temp = new Trie;
-            ma.insert({v[idx], temp});
-        }
-        ma[v[idx]]->insert(v, idx + 1);
+        for (int i = 0; i < 2; i++)
+            if (node[i])
+                delete node[i];
     }
 
-    void search(int depth, string &res)
+    void insert(bitset<32> &bits, int idx)
     {
-        char now = res[depth];
-        char target = (now - '0' + 1) % 2 + '0';
-        if (ma.size() == 0)
+        if (idx == -1)
             return;
-        if (ma.find(target) == ma.end())
+
+        if (node[bits[idx]] == NULL)
         {
-            res[depth] = '0';
-            ma[now]->search(depth + 1, res);
+            Trie *temp = new Trie;
+            node[bits[idx]] = temp;
+        }
+        node[bits[idx]]->insert(bits, idx - 1);
+    }
+
+    void search(bitset<32> &bits, int idx)
+    {
+        if (idx == -1)
+            return;
+        int now = bits[idx];
+        int target = (now + 1) % 2;
+
+        if (node[target])
+        {
+            bits[idx] = 1;
+            node[target]->search(bits, idx - 1);
         }
         else
         {
-            res[depth] = '1';
-            ma[target]->search(depth + 1, res);
+            bits[idx] = 0;
+            node[now]->search(bits, idx - 1);
         }
     }
 };
-
-int bin_to_decimal(string &s)
-{
-    int res = 0;
-    for (int i = 0; i < s.length(); i++)
-    {
-        int idx = s.length() - 1 - i;
-
-        if (s[idx] == '0')
-            continue;
-        res += (1 << i);
-    }
-    return res;
-}
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -71,28 +65,20 @@ int main()
     {
         int n;
         cin >> n;
-        vector<int> v(n + 1);
-        int ans = 0;
         Trie *root = new Trie;
+        vector<int> v(n + 1);
+        bitset<32> zero(0);
+        root->insert(zero, 31);
 
-        vector<char> init(32);
-        for (int i = 0; i < 32; i++)
-            init[i] = '0';
-        root->insert(init, 0);
+        int ans = 0;
         for (int i = 1; i <= n; i++)
         {
             cin >> v[i];
             v[i] ^= v[i - 1];
-
             bitset<32> bits(v[i]);
-            vector<char> String;
-            string s = bits.to_string();
-            for (int j = 0; j < s.length(); j++)
-                String.push_back(s[j]);
-
-            root->insert(String, 0);
-            root->search(0, s);
-            int res = bin_to_decimal(s);
+            root->insert(bits, 31);
+            root->search(bits, 31);
+            int res = bits.to_ulong();
             ans = max(ans, res);
         }
 
