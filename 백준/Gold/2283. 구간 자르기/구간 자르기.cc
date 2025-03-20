8b45097df416624c03cbe4a9b070f496ac6b918a
g0729 <151703074+g0729@users.noreply.github.com>
2025-03-20 19:47:02 +0900

    [Gold II] Title: 구간 자르기, Time: 4 ms, Memory: 6068 KB -BaekjoonHub

--- 백준/Gold/2283. 구간 자르기/구간 자르기.cc
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.cc
@@ -14,14 +14,30 @@ int main()
     cout.tie(0);
     int n, k;
     cin >> n >> k;
+    vector<pair<int, int>> points;
     vector<int> v(MAX);
     for (int i = 0; i < n; i++)
     {
         int a, b;
         cin >> a >> b;
-        for (int j = a; j < b; j++)
-            v[j]++;
+        points.push_back({a, 1});
+        points.push_back({b, -1});
     }
+
+    sort(points.begin(), points.end(), [](const auto &a, const auto &b)
+         {
+        if(a.first==b.first)
+            return a.second>b.second;
+        return a.first<b.first; });
+
+    int cur = 1;
+    for (int i = 1; i < points.size(); i++)
+    {
+        for (int j = points[i - 1].first; j < points[i].first; j++)
+            v[j] += cur;
+        cur += points[i].second;
+    }
+
     int left = 0, right = 0, sum = 0;
 
     while (left <= right && right < MAX)
