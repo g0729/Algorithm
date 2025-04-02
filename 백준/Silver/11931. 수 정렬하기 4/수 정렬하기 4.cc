59910cadd383dbc274dbe2bf6326cd52ee192e8d
g0729 <151703074+g0729@users.noreply.github.com>
2025-04-02 18:09:53 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 256 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
 #define INF 987654321
-#define lint long long
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	int n;
-	cin >> n;
-	vector<int> v(n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> v[i];
-	}
-	
-	sort(v.begin(), v.end(), greater<int>());
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	for (int i = 0; i < n; i++)
-	{
-		cout << v[i] << "\n";
-	}
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
 
-	return 0;
+    sort(v.begin(), v.end(), greater<int>());
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    return 0;
 }
