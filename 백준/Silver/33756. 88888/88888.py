69658477c5c8ec8447f7003f0aa419ef3d22398b
g0729 <151703074+g0729@users.noreply.github.com>
2025-04-09 20:26:28 +0900

    [Silver III] Title: 88888, Time: 152 ms, Memory: 110760 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/33756. 88888/88888.py
+++ 백준/Silver/33756. 88888/88888.py
@@ -1,5 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
 MAX = 1000000000000000000
 v = [8]
+
+
 while v[-1] < MAX:
     v.append(v[-1] * 10 + 8)
 
@@ -9,7 +16,7 @@ for _ in range(int(input())):
     n = int(input())
 
     cnt, idx = 0, 0
-    while n != 0 and cnt <= 8:
+    while n != 0 and cnt <= 8 and idx < len(v):
 
         if v[idx] <= n:
             n -= v[idx]
@@ -19,4 +26,4 @@ for _ in range(int(input())):
         if n == 0:
             break
 
-    print("Yes" if cnt <= 8 else "No")
+    print("Yes\n" if cnt <= 8 and n == 0 else "No\n")
