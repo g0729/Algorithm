6dcb2eb555f9e69e2966522a8482960ef2d3898e
g0729 <151703074+g0729@users.noreply.github.com>
2025-05-14 18:07:52 +0900

    [Platinum I] Title: 마피아, Time: 48 ms, Memory: 5456 KB -BaekjoonHub

---
+++ 백준/Platinum/1210. 마피아/마피아.cc
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 500
+#define INF 987654321
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+
+struct Edge
+{
+    int next, capacity, flow;
+    Edge *rev;
+
+    Edge(int next, int capacity) : next(next), capacity(capacity), flow(0), rev(nullptr) {};
+    int residual() { return capacity - flow; };
+    void update(int flow)
+    {
+        this->flow += flow;
+        this->rev->flow -= flow;
+    }
+};
+int n, m;
+vector<vector<Edge *>> edges(MAX, vector<Edge *>());
+int source, sink;
+void add_edge(int from, int to, int capacity)
+{
+    Edge *e1 = new Edge(to, capacity);
+    Edge *e2 = new Edge(from, 0);
+
+    e1->rev = e2;
+    e2->rev = e1;
+
+    edges[from].push_back(e1);
+    edges[to].push_back(e2);
+}
+
+void mcmf()
+{
+    while (true)
+    {
+        queue<int> q;
+        q.push(source);
+        vector<pair<int, Edge *>> parent(MAX, {-1, nullptr});
+
+        while (!q.empty() && parent[sink].first == -1)
+        {
+            int cur = q.front();
+            q.pop();
+
+            for (auto edge : edges[cur])
+            {
+                int next = edge->next;
+
+                if (edge->residual() && parent[next].first == -1)
+                {
+                    q.push(next);
+                    parent[next] = {cur, edge};
+                }
+            }
+        }
+
+        if (parent[sink].first == -1)
+            break;
+
+        int min_flow = INF;
+
+        for (int i = sink; i != source; i = parent[i].first)
+            min_flow = min(min_flow, parent[i].second->residual());
+
+        for (int i = sink; i != source; i = parent[i].first)
+            parent[i].second->update(min_flow);
+    }
+}
+
+void bfs(vector<bool> &reachable)
+{
+    queue<int> q;
+    q.push(source);
+    reachable[source] = true;
+
+    while (!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+
+        for (auto edge : edges[cur])
+        {
+            int next = edge->next;
+
+            if (edge->residual() && !reachable[next])
+            {
+                reachable[next] = true;
+                q.push(next);
+            }
+        }
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+
+    cin >> source >> sink;
+    source = source * 2 - 1,
+    sink = sink * 2;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int a;
+        cin >> a;
+        int in = i * 2 - 1, out = in + 1;
+        add_edge(in, out, a);
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        int u_in = u * 2 - 1, u_out = u_in + 1, v_in = v * 2 - 1, v_out = v_in + 1;
+
+        add_edge(u_out, v_in, INF);
+        add_edge(v_out, u_in, INF);
+    }
+
+    mcmf();
+
+    vector<bool> reachable(MAX, false);
+    bfs(reachable);
+
+    for (int i = 1; i <= n; i++)
+    {
+        int in = i * 2 - 1, out = in + 1;
+
+        if (reachable[in] && !reachable[out])
+        {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
