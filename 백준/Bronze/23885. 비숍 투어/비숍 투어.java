904da91a22f199cea82733faeff2272132ddb35c
g0729 <151703074+g0729@users.noreply.github.com>
2025-06-03 15:14:46 +0900

    [Bronze I] Title: 비숍 투어, Time: 68 ms, Memory: 11528 KB, Score: 35 point -BaekjoonHub

---
+++ 백준/Bronze/23885. 비숍 투어/비숍 투어.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken()), d = Integer.parseInt(st.nextToken());
+
+        if ((a + b) % 2 == (c + d) % 2)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+        br.close();
+    }
+}
