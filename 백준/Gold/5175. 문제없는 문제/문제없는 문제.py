8d376e8d6900c466c759d440e4e5779847943390
g0729 <151703074+g0729@users.noreply.github.com>
2025-07-04 18:16:13 +0900

    [Gold V] Title: 문제없는 문제, Time: 104 ms, Memory: 110576 KB -BaekjoonHub

---
+++ 백준/Gold/5175. 문제없는 문제/문제없는 문제.py
@@ -0,0 +1,30 @@
+for test_case in range(int(input())):
+
+    m, n = map(int, input().split())
+    v = [0] * n
+    select_all = (1 << m) - 1
+    for i in range(n):
+        for a in map(int, input().split()):
+            v[i] |= 1 << (a - 1)
+
+    res_cnt = 30
+    res = 0
+    for mask in range(1, (1 << n)):
+        cur_mask = 0
+        cnt = 0
+        for i in range(n):
+            if (mask >> i) & 1:
+                cnt += 1
+                cur_mask |= v[i]
+
+        if cur_mask == select_all:
+            if res_cnt > cnt:
+                res_cnt = cnt
+                res = mask
+
+    print(f"Data Set {test_case+1}: ", end="")
+
+    for idx, cur in enumerate(bin(res)[2:][::-1]):
+        if cur == "1":
+            print(chr(idx + ord("A")), end=" ")
+    print("\n")
