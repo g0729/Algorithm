2a33116663b898e94b1d9cd93d076f512c938a05
g0729 <151703074+g0729@users.noreply.github.com>
2025-08-12 14:45:37 +0900

    [Gold V] Title: Quilting (Large), Time: 108 ms, Memory: 110896 KB -BaekjoonHub

--- 백준/Gold/14597. Quilting （Large）/Quilting （Large）.py
+++ 백준/Gold/14597. Quilting （Large）/Quilting （Large）.py
@@ -7,23 +7,11 @@ def solve():
     h, w = map(int, input().split())
     b1 = [list(map(int, input().split())) for _ in range(h)]
     b2 = [list(map(int, input().split())) for _ in range(h)]
-    dp = [[0] * w for _ in range(h)]
-
-    for i in range(w):
-        dp[0][i] = (b1[0][i] - b2[0][i]) ** 2
+    dp = [[(b1[i][j] - b2[i][j]) ** 2 for j in range(w)] for i in range(h)]
 
     for i in range(1, h):
         for j in range(w):
-            dp[i][j] = (b1[i][j] - b2[i][j]) ** 2
-            if w == 1:
-                dp[i][j] += dp[i - 1][j]
-                continue
-            if j == 0:
-                dp[i][j] += min(dp[i - 1][j], dp[i - 1][j + 1])
-            elif j == w - 1:
-                dp[i][j] += min(dp[i - 1][j], dp[i - 1][j - 1])
-            else:
-                dp[i][j] += min(dp[i - 1][j - 1], dp[i - 1][j], dp[i - 1][j + 1])
+            dp[i][j] += min(dp[i - 1][max(0, j - 1) : min(w, j + 2)])
 
     print(min(dp[h - 1]))
 
