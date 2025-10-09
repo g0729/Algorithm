fbaab2f78c125098c30cfd1e4cd7e9aea31c3cff
g0729 <151703074+g0729@users.noreply.github.com>
2025-10-09 16:26:29 +0900

    [Gold V] Title: 피타고라스 정리의 증명, Time: 348 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/33466. 피타고라스 정리의 증명/피타고라스 정리의 증명.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, -1, 1};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        ll a;
+        cin >> a;
+        cout << a * 2 - (a % 2 ? 1 : 0) << "\n";
+    }
+    return 0;
+}
