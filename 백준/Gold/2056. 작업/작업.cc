62b60d4769c80f3272c3ae10289c7cfe913b338c
g0729 <151703074+g0729@users.noreply.github.com>
2025-10-16 18:48:29 +0900

    [Gold IV] Title: 작업, Time: 60 ms, Memory: 5960 KB -BaekjoonHub

---
+++ 백준/Gold/2056. 작업/작업.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, -1, 1};
+
+int n;
+vector<int> costs(10001);
+vector<int> edges[10001];
+vector<int> in_degree(10001);
+
+void get_in_degree() {
+    for (int i = 1; i <= n; i++) {
+        for (auto &next : edges[i]) in_degree[next]++;
+    }
+}
+
+int topo_sort() {
+    get_in_degree();
+    queue<int> q;
+    vector<int> dp(n + 1);
+
+    for (int i = 1; i <= n; i++) {
+        if (!in_degree[i]) {
+            q.push(i);
+            dp[i] = costs[i];
+        }
+    }
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        for (auto &next : edges[cur]) {
+            in_degree[next]--;
+            dp[next] = max(dp[next], dp[cur] + costs[next]);
+
+            if (!in_degree[next]) q.push(next);
+        }
+    }
+
+    int res = *max_element(dp.begin(), dp.end());
+    return res;
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> costs[i];
+        int a;
+        cin >> a;
+        for (int b; a-- && cin >> b;) {
+            edges[b].push_back(i);
+        }
+    }
+
+    cout << topo_sort();
+    return 0;
+}
