d04e602930e43b03674fee57c5c51028a0cd607f
g0729 <151703074+g0729@users.noreply.github.com>
2025-10-25 15:40:30 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    while (!cin.eof())
+    {
+        string s;
+        getline(cin, s);
+        cout << s<<"\n";
+    }
+    return 0;
+}
