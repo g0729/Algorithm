82415b7e91fca2cd6afc219597529c7f1bc6a336
g0729 <151703074+g0729@users.noreply.github.com>
2025-11-21 20:34:34 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+sys.setrecursionlimit(10**6)
+
+
+def solve():
+    v = {
+        1: (11, "A B C D E F G H J L M"),
+        2: (9, "A C E F G H I L M"),
+        3: (9, "A C E F G H I L M"),
+        4: (9, "A B C E F G H L M"),
+        5: (8, "A C E F G H L M"),
+        6: (8, "A C E F G H L M"),
+        7: (8, "A C E F G H L M"),
+        8: (8, "A C E F G H L M"),
+        9: (8, "A C E F G H L M"),
+        10: (8, "A B C F G H L M"),
+    }
+
+    print("\n".join(map(str, v[int(input())])))
+
+
+if __name__ == "__main__":
+    solve()
