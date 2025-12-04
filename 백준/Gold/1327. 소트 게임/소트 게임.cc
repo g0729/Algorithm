dcbec488c719ff6aae68c206aaeb02303754e6e8
g0729 <151703074+g0729@users.noreply.github.com>
2025-12-04 16:38:42 +0900

    [Gold IV] Title: 소트 게임, Time: 92 ms, Memory: 7180 KB -BaekjoonHub

--- 백준/Gold/1327. 소트 게임/소트 게임.cc
+++ 백준/Gold/1327. 소트 게임/소트 게임.cc
@@ -7,48 +7,33 @@ using namespace std;
 int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
 int dy[] = {0, 0, 1, -1, -1, 1, -1, 1};
 
-map<vector<int>, int> ranks;
+map<vector<int>, int> dist;
 int n, k;
 
-bool check(vector<int> &v) {
-
-    for (int i = 1; i < v.size(); i++) {
-        if (v[i - 1] > v[i]) return false;
-    }
-
-    return true;
-}
-
 int bfs(vector<int> start) {
+
+    if (is_sorted(start.begin(), start.end())) return 0;
+
     queue<vector<int>> q;
     q.push(start);
-    vector<bool> visited(MAX);
-    visited[ranks[start]] = true;
-    int res = 0;
+    dist[start] = 1;
+
     while (!q.empty()) {
+        vector<int> cur = q.front();
+        q.pop();
 
-        int size = q.size();
-        while (size--) {
-            vector<int> cur = q.front();
-            q.pop();
+        if (is_sorted(cur.begin(), cur.end())) return dist[cur] - 1;
 
-            if (check(cur)) return res;
+        for (int i = 0; i <= n - k; i++) {
+            vector<int> next = cur;
 
-            for (int i = 0; i <= n - k; i++) {
-                vector<int> temp = cur;
+            reverse(next.begin() + i, next.begin() + i + k);
 
-                for (int j = 0; j < k / 2; j++) {
-                    swap(temp[i + j], temp[i + k - j - 1]);
-                }
-
-                if (visited[ranks[temp]]) continue;
-
-                q.push(temp);
-                visited[ranks[temp]] = true;
+            if (dist.find(next) == dist.end()) {
+                dist[next] = dist[cur] + 1;
+                q.push(next);
             }
         }
-
-        res++;
     }
 
     return -1;
@@ -60,15 +45,9 @@ int main() {
     cout.tie(0);
 
     cin >> n >> k;
-    vector<int> v;
-    for (int i = 1; i <= n; i++) v.push_back(i);
-
-    int rank = 1;
-    do {
-        ranks[v] = rank++;
-    } while (next_permutation(v.begin(), v.end()));
 
     vector<int> start(n);
+
     for (auto &a : start) cin >> a;
 
     cout << bfs(start);
