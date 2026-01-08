69a1d5f41242a1bb97fab8a566141727798105f3
g0729 <151703074+g0729@users.noreply.github.com>
2026-01-08 22:16:16 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 72 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        System.out.println((a.equals(b) ? 0 : 1550));
+        br.close();
+    }
+}
