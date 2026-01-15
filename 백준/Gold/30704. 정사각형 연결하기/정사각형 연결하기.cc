67a024928cff96f1fad1a448cbcb2d5cb5f66a22
g0729 <151703074+g0729@users.noreply.github.com>
2026-01-15 16:55:57 +0900

    [Gold V] Title: 정사각형 연결하기, Time: 20 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/30704. 정사각형 연결하기/정사각형 연결하기.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, -1, 1};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int a = sqrt(n);
+        int b = ceil((double)n / a);
+        cout << (a + b) * 2;
+        cout << "\n";
+    }
+
+    return 0;
+}
