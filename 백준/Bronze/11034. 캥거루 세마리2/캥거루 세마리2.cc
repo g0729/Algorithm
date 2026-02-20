3596413213c2ce46b454984940c860c9d1bfdf6f
g0729 <151703074+g0729@users.noreply.github.com>
2026-02-20 17:04:25 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, -1, 1};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a, b, c;
+    while (cin >> a >> b >> c) cout << max(b - a - 1, c - b - 1) << endl;
+    return 0;
+}
