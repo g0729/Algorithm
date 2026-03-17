0f1ff9b1358a539a9eef6ee55b06040d71bede34
g0729 <151703074+g0729@users.noreply.github.com>
2026-03-17 20:27:55 +0900

    [Bronze IV] Title: SciComLove (2025), Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/33810. SciComLove （2025）/SciComLove （2025）.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+#define MAX 100000
+#define INF 987654321
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, -1, 1};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string a = "SciComLove";
+    string b;
+    cin >> b;
+    int res = 0;
+    for (int i = 0; i < a.length(); i++) {
+        if (a[i] != b[i]) res++;
+    }
+
+    cout << res;
+    return 0;
+}
