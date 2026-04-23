ac1287d916f50d676b51babaff001cd3d91a5fc0
g0729 <151703074+g0729@users.noreply.github.com>
2026-04-23 15:27:14 +0900

    Title: 100점 도달하기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260423/100점 도달하기/reach-100-point.py
@@ -0,0 +1,17 @@
+def f(N :int):
+    if N >= 90:
+        return 'A'
+    elif N >= 80:
+        return 'B'
+    elif N>=70:
+        return 'C'
+    elif N>=60:
+        return 'D'
+    else:
+        return F
+
+
+n=int(input())
+
+for i in range(n,101):
+    print(f(i),end=' ')
