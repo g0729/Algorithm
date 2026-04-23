ffd4aff81e81530ac4cab8dbe476179028914047
g0729 <151703074+g0729@users.noreply.github.com>
2026-04-23 15:36:33 +0900

    Title: 친근하지 않은 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260423/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int res = n;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) {
+                res--;
+            }
+        }
+
+        System.out.println(res);
+        br.close();
+    }
+}
