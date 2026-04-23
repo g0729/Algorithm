c7a078807e41ef1117c888925ea2457b68eccdcf
g0729 <151703074+g0729@users.noreply.github.com>
2026-04-23 16:08:18 +0900

    Title: 0 나올때까지 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260423/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            if (n == 0)
+                break;
+            System.out.println(n);
+
+        }
+        br.close();
+    }
+}
