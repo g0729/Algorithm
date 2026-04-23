163667f27b7aac128c856943ab968b507abb37c7
g0729 <151703074+g0729@users.noreply.github.com>
2026-04-23 15:38:11 +0900

    Title: 1부터의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260423/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+
+            if (sum >= n) {
+                System.out.println(i);
+                break;
+            }
+        }
+        br.close();
+    }
+}
